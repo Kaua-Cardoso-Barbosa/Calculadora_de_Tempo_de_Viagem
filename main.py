055cc8112cefb048d531b19db6c853843396a13c
Kaua-Cardoso-Barbosa <kauacardosobarbosasenai@gmail.com>
2025-04-25 16:27:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
-
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/viagem', methods=['POST'])
+def viagem():
+    try:
+        distancia = float(request.form['distancia'])
+        velocidade = float(request.form['velocidade'])
+        total = round((distancia/velocidade),2)
+        tempo_s = int(total * 3600)
+        horas = int(tempo_s / 3600)
+        tempo_s = int(tempo_s % 3600)
+        minutos = int(tempo_s / 60)
+        return render_template('index.html',horas=horas , minutos=minutos)
+    except Exception as e:
+        total = f'Ocorreu um erro inesperado {e}'
+        return render_template('index.html',horas=horas, minutos=minutos)
 
-@app.route('/calcular_hora', methods=['POST'])
-def calcular_hora():
-    distancia = float(request.form['distancia'])
-    velocidade = float(request.form['velocidade'])
-
-    tempo = round(distancia/velocidade,2)
-    return render_template('index.html', tempo=tempo)
-
-if __name__ == '__main__':
+if __name__=='__main__' :
     app.run(debug=True)
